a2fa19da2c28ace2d6993a1490d17e05c9613786
WillShine90 <821883375@qq.com>
2025-02-12 15:51:35 +0800

    V3.0 添加xIsNext布尔值以实现交替落子；添加避免已被填写的方块再次被改写的机制

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 🏗 This app is built using the React framework.
 
-📂 v1.0 构建UI，每次点击Square显示X
+📂 V1.0 构建UI，每次点击Square显示X
 
-   v2.0 前端看起来与v1.0并没有什么不同，但是引入了squares数组，每次点击Square时，创建 squares 数组的副本，支持撤消和重做某些操作
+   V2.0 前端看起来与v1.0并没有什么不同，但是引入了squares数组，每次点击Square时，创建 squares 数组的副本，支持撤消和重做某些操作
+
+   V3.0 添加xIsNext布尔值以实现交替落子；添加避免已被填写的方块再次被改写的机制
--- src/App.js
+++ src/App.js
@@ -14,18 +14,35 @@ function Square({value,onSquareClick}){
 
 // 定义主组件 Board
 export default function Board() {
-  // 声明一个名为 squares 的 state 变量，该变量默认为对应于 9 个方块的 9 个空值数组
+  //声明一个名为 xIsNext 的 state 变量，来设置第一次点击标记为 X，每次玩家落子时，xIsNext 将被翻转以确定下一个玩家
+  const [xIsNext, setXIsNext] = useState(true);
+  // 声明一个名为 squares 的 state 变量，该变量默认为对应于 9 个方块的 9 个空值数组，setSquares 函数用来刷新 squares值
   const [squares,setSquares]=useState(Array(9).fill(null))
 
   // 定义 handleClick 函数来更新并保存棋盘 state 的 squares 数组
   function handleClick(i) {
+
+    // 检查方块是否已经有 X 或 O，如果不为 null，不更新 squares
+    if (squares[i]) {
+      return;
+    }
+
     // 声明 nextSquares 数组，用来储存更新的 squares 数组
     const nextSquares = squares.slice()
-    // 将索引为 i 的 squares 内的元素从 null 更新为 X
-    nextSquares[i] = "X";
+    
+    if (xIsNext) {
+      // 如果 xIsNext=true 将索引为 i 的 squares 内的元素从 null 更新为 X
+      nextSquares[i] = "X"; 
+    } else {
+      // 如果 xIsNext=false 将索引为 i 的 squares 内的元素从 null 更新为 O
+      nextSquares[i] = "O"; 
+    }
     // 调用 setSquares 函数让 React 知道组件的 squares state 已经改变
     setSquares(nextSquares);
+    // 翻转布尔值
+    setXIsNext(!xIsNext);
   }
+   
 
   return (
     <>
